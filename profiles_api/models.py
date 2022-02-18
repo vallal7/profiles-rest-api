bcc878392f644d86fa37dfbd0cdb66bee77da7db
Ashwin Ganesh <ashwin.ganesh@weightwatchers.com>
2022-02-18 13:00:00 -0500

    typo

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -42,7 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieves user full name"""
